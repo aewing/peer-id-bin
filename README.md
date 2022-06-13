9d6c74ad433513599cbdabe5e5e56564209a5c83
Drew Ewing <drew.ewing@satellite.im>
2022-06-13 13:54:11 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# peer-id-bin
+
+a peer-id binary for node lts+
+
+## Usage
+
+```sh
+pnpm install peer-id-bin
+pnpm peer-id-bin --type=ed25519 > peerid.json
+```
---
+++ index.js
@@ -0,0 +1,33 @@
+import PeerId from "peer-id";
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv.privateKey) {
+  const peerId = await PeerId.createFromPrivKey(argv.privateKey);
+  conclude(peerId);
+}
+
+if (argv.hex) {
+  const peerId = await PeerId.createFromHexString(argv.hex);
+  conclude(peerId);
+}
+
+if (argv.publicKey) {
+  const peerId = await PeerId.createFromPubKey(argv.publicKey);
+  conclude(peerId);
+}
+
+if (argv.cid) {
+  const peerId = await PeerId.createFromCID(argv.cid);
+  conclude(peerId);
+}
+
+const type = argv.type || "ed25519";
+const peerId = await PeerId.create({ type });
+conclude(peerId);
+
+function conclude(peerId) {
+  console.log(peerId.toJSON());
+  process.exit(0);
+}
